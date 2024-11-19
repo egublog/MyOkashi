1d06fad57c9904c42fd2bf66755894a324224785
egublog <egukei0729@gmail.com>
2024-11-20 01:16:37 +0900

    Initial Commit

---
+++ MyOkashi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyOkashi
+//
+//  Created by 江口敬太郎 on 2024/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by 江口敬太郎 on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
