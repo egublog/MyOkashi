c243c573bfb080616f001dce44db26c4b6fb152e
egublog <egukei0729@gmail.com>
2024-11-23 22:56:04 +0900

    create: MyOkashi/SafariView.swift

---
+++ MyOkashi/SafariView.swift
@@ -0,0 +1,18 @@
+//
+//  SafariView.swift
+//  MyOkashi
+//
+//  Created by 江口敬太郎 on 2024/11/23.
+//
+
+import SwiftUI
+
+struct SafariView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafariView()
+}
